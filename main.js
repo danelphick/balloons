9b8bf7ef36120c45d1ba054bc29e786561d90434
Dan Elphick <delphick@google.com>
2020-10-11 17:30:41 +0100

    Add an actual game

--- main.js
+++ main.js
@@ -1,1 +1,154 @@
-canvas = document.getElementById('game');
+'use strict';
+
+var canvas = null;
+var pop_sound = null;
+const colors = ['red', 'blue', 'green', 'yellow', 'brown', 'purple', 'pink',
+                'gray', 'orange', 'aqua', 'cornflowerblue', 'crimson',
+                'darkred', 'darkseagreen', 'deepskyblue', 'greenyellow',
+                'indigo', 'maroon', 'lightslategrey', 'mediumorchid', 'moccasin',
+                'teal', 'tan'];
+
+function randomColor() {
+  return colors[Math.floor(Math.random() * colors.length)];
+}
+
+class Balloon {
+  constructor(x, color) {
+    this.height = Math.pow(Math.random(),3) * 80 + 40;
+    this.width = Math.pow(Math.random(), 2) * (this.height - 20) + 20;
+    this.x = x;
+    this.y = canvas.height + this.height;
+    this.color1 = randomColor();
+    this.color2 = randomColor();
+    this.text = String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+    
+    this.speed = Math.random() + 0.5;
+    let len = Math.random() * 50 + 50;
+    this.string = [
+        [(Math.random() - 0.5) * 30, len / 3],
+        [(Math.random() - 0.5) * 30, 2 * len / 3],
+        [(Math.random() - 0.5) * 30, len],
+    ];
+  }
+}
+
+var canvas = null;
+var balloons = [];
+var wind = 0;
+var maxWindChange = 0.5;
+var shoot = null;
+var lastSpawnTime = Date.now();
+const twoPi = Math.PI * 2;
+const spawnTime = 2000;
+
+function drawBalloon(ctx, balloon) {
+  ctx.globalCompositeOperation = 'source-over'
+  ctx.fillStyle = balloon.color1;
+
+  ctx.beginPath();
+  ctx.ellipse(balloon.x, balloon.y, balloon.width, balloon.height, 0, 0, twoPi);
+  ctx.fill();
+  ctx.strokeStyle = balloon.color2;
+  ctx.beginPath();
+  ctx.moveTo(balloon.x, balloon.y + balloon.height);
+  let s = balloon.string;
+  ctx.bezierCurveTo(
+       balloon.x + s[0][0], balloon.y + balloon.height + s[0][1],
+       balloon.x + s[1][0], balloon.y + balloon.height + s[1][1],
+       balloon.x + s[2][0], balloon.y + balloon.height + s[2][1],
+    );
+  ctx.stroke();
+
+  ctx.fillStyle = 'black';
+  ctx.globalCompositeOperation = 'plus-darker'
+  ctx.fillText(balloon.text, balloon.x, balloon.y);
+}
+
+function spawnBalloon() {
+  let x = Math.floor(Math.random() * canvas.width);
+  let b = new Balloon(x);
+  balloons.push(b);
+}
+
+function run() {
+  var ctx = canvas.getContext("2d");
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.textAlign = 'center';
+  ctx.textBaseline = 'middle';
+  ctx.font = 'bold 48px serif';
+  let newBalloons = [];
+  let current = null;
+  let currentIndex = 0;
+
+  let currentTime = Date.now();
+  if (currentTime - spawnTime > lastSpawnTime) {
+    spawnBalloon();
+    lastSpawnTime = currentTime;
+  }
+
+  if (shoot != null) {
+    for (let i = 0; i < balloons.length; ++i) {
+      let balloon = balloons[i];
+      if (balloon.text == shoot) {
+        if (current == null || (balloon.y + balloon.height > current.y + current.height)) {
+          current = balloon;
+          currentIndex = i;
+        }
+      }
+    }
+    shoot = null;
+  }
+
+  if (current != null) {
+    pop_sound.currentTime = 0;
+    pop_sound.play();
+    balloons.splice(currentIndex, 1);
+  }
+    
+  for (let i = 0; i < balloons.length; ++i) {
+    let balloon = balloons[i];
+    drawBalloon(ctx, balloon);
+    let localWind = wind * (1.2 - 0.4 * Math.random()); 
+    balloon.x = Math.min(Math.max(balloon.x + localWind, balloon.width), canvas.width - balloon.width);
+    balloon.y -= balloon.speed;
+    if (balloon.y + balloon.height + balloon.string[2][1] > 0) {
+      newBalloons.push(balloon);
+    }  
+  }
+  wind += (Math.random() - 0.5) * maxWindChange;
+  const maxWind = 1;
+  wind = Math.min(wind, maxWind);
+  wind = Math.max(wind, -maxWind);
+  balloons = newBalloons;
+  window.requestAnimationFrame(run)
+}
+
+function init() {
+  function enterListener(e) {
+    if (e.code == 'Enter') {
+      gameInit();
+      document.getElementById('start_prompt').style.display = "none";
+      window.removeEventListener("keypress", enterListener);
+    }
+  }
+  window.addEventListener("keypress", enterListener, false);
+}
+
+function gameInit() {
+  window.addEventListener("keydown",
+    function(e) {
+      let digit = +e.key;
+      if (!isNaN(digit)) {
+        shoot = digit;
+      }
+    },
+    false);
+
+  canvas = document.getElementById('game');
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  pop_sound = document.getElementById('pop_sound');
+  run();
+}
+
